cb649b33d8a726c42029d826c61c43251d7b450c
azhdaha <azhdaha@gmail.com>
2025-05-29 05:23:56 +0000

    Complete authentication system and prepare for production branch

--- backend/generateHash.js
+++ backend/generateHash.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
-const passwordToHash = 'wkwkland123'; // Change this to your desired password
+const passwordToHash = 'hashpassword'; // Change this to your desired password
 const saltRounds = 10;
 const hashedPassword = bcrypt.hashSync(passwordToHash, saltRounds);
-console.log(`Username: maw`); // Replace yourNewUsername
+console.log(`Username: username`); // Replace yourNewUsername
 console.log(`Password: ${passwordToHash}`);
 console.log(`Hashed Password: ${hashedPassword}`);
